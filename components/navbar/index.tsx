ac90b8d84c16ad648d3c60305d935eda88921bdc
Marcelo Espinoza <65143517+Marcel-19@users.noreply.github.com>
2024-10-31 20:23:05 -0500

    Navbar mejorada
    Se mejoró el navbar ya que ahora no se muestra directamente el cerrar sesión, si no que aparece después dentro de un dropdown menú.

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -3,9 +3,23 @@
 import { Button, buttonVariants } from '@/components/ui/button';
 import { signOut } from 'next-auth/react'; // Client-side signOut
 import Image from 'next/image';
-import { LogIn, LogOut } from "lucide-react";
+import { LogIn, LogOut, User, Settings, Library } from "lucide-react";
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
 
 export default function Navbar() {
   const { data: session } = useSession(); // Use the session on the client side
@@ -13,26 +27,52 @@ export default function Navbar() {
 
   if (!user) {
     return (
-      <Link className={buttonVariants({ variant: "outline" })} href="/signin">
-        <LogIn className="mr-2 h-4 w-4" />
-        Iniciar Sesión
-      </Link>
+      <Button variant="outline" asChild>
+        <Link href="/signin">
+          <LogIn className="mr-2 h-4 w-4" />
+          Iniciar Sesión
+        </Link>
+      </Button>
     );
   }
 
   return (
-    <div className="flex items-center gap-4">
-      <Button variant="outline" onClick={() => signOut()}>
-        <LogOut className="mr-2 h-4 w-4" /> 
-        Cerrar Sesión
-      </Button>
-      <Image
-        className="h-8 w-8 rounded-full"
-        src={user.image!}
-        height={32}
-        width={32}
-        alt={`${user.name} avatar`}
-      />
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="relative h-8 w-8 rounded-full right-2">
+          <Avatar className="h-8 w-8">
+            {user.image ? (
+              <AvatarImage src={user.image} alt={user.name || ''} />
+            ) : (
+              <AvatarFallback className='bg-yellow-900'>
+                <Library className="h-4 w-4 text-yellow-300" />
+              </AvatarFallback>
+            )}
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{user.name}</p>
+            <p className="text-xs leading-none text-muted-foreground">{user.email}</p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <Link href="/perfil">
+          <DropdownMenuItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>Perfil</span>
+          </DropdownMenuItem>
+          </Link>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => signOut()}>
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Cerrar Sesión</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
